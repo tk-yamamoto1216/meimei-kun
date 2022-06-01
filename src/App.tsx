210cb7bb7e7b6796dea7dbd75050fb0e97357464
Takenari Yamamoto <takenari_yamamoto1216@arsaga.jp>
2022-06-01 09:40:57 +0900

    add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Button from "@mui/material/Button";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
 
 function App() {
+  const options = [
+    {
+      en: "register",
+      jp: "登録する",
+    },
+    {
+      en: "post",
+      jp: "投稿する",
+    },
+    {
+      en: "add",
+      jp: "追加する",
+    },
+  ];
+
+  const handleChange = (e: SelectChangeEvent) => {
+    console.log(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>命名君</p>
-      </header>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value="選択してね"
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
