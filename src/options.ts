d7d5c2978ab801112a3717c06671e0d3a72b2655
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-06-18 17:26:25 +0900

    add options

--- src/options.ts
+++ src/options.ts
@@ -1,41 +1,89 @@
 export const processOptions = [
   {
-    en: "register",
-    jp: "登録する",
+    en: 'register',
+    jp: '登録する',
   },
   {
-    en: "find",
-    jp: "探す",
+    en: 'post',
+    jp: '投稿する',
   },
   {
-    en: "post",
-    jp: "投稿する",
+    en: 'add',
+    jp: '追加する',
   },
   {
-    en: "add",
-    jp: "追加する",
+    en: 'insert',
+    jp: '挿入する',
   },
   {
-    en: "delete",
-    jp: "削除する",
+    en: 'fetch',
+    jp: '（外部から）取得する',
   },
   {
-    en: "rehabilitate",
-    jp: "厚生する",
+    en: 'get',
+    jp: '（内部から）取得する',
+  },
+  {
+    en: 'to',
+    jp: '変換する',
+  },
+  {
+    en: 'find',
+    jp: '探す',
+  },
+  {
+    en: 'update',
+    jp: '更新する',
+  },
+  {
+    en: 'change',
+    jp: '変更する',
+  },
+  {
+    en: 'replace',
+    jp: '入れ替える',
+  },
+  {
+    en: 'merge',
+    jp: '結合する',
+  },
+  {
+    en: 'return',
+    jp: '返す',
+  },
+  {
+    en: 'delete',
+    jp: '削除する',
+  },
+  {
+    en: 'clear',
+    jp: '空にする',
+  },
+  {
+    en: 'format',
+    jp: '整形する',
+  },
+  {
+    en: 'toggle',
+    jp: '切り替える',
+  },
+  {
+    en: 'transition',
+    jp: '遷移する',
   },
 ];
 
 export const prepositionOptions = [
   {
-    en: "no",
-    jp: "なし",
+    en: 'no',
+    jp: 'なし',
   },
   {
-    en: "from",
-    jp: "~から",
+    en: 'from',
+    jp: '~から',
   },
   {
-    en: "by",
-    jp: "~を用いて",
+    en: 'by',
+    jp: '~を用いて',
   },
 ];
