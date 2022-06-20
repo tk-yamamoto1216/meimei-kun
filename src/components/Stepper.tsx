86338045ba7dfee9e88a8d5438f8512c4cd11727
ChisatoShiraga <chisatoshiraga@A169.local>
2022-06-20 10:54:00 +0900

    videoタグにplaysInlineを追加

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -86,6 +86,7 @@ export default function Stepper() {
             className="video"
             autoPlay
             loop
+            playsInline
             width={`80%`}
             height={`80%`}
           ></video>
